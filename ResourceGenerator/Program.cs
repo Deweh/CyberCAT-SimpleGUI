f44130b1ef07bed3d379d9e59aa89a26271e35f3
seberoth <seberoth@users.noreply.github.com>
2022-10-21 22:06:33 +0200

    Added more generators

--- ResourceGenerator/Program.cs
+++ ResourceGenerator/Program.cs
@@ -1,7 +1,10 @@
-﻿using System.Text.Json;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Text.Json;
 using WolvenKit.Common.Services;
 using WolvenKit.RED4.Archive.IO;
 using WolvenKit.RED4.TweakDB;
+using WolvenKit.RED4.TweakDB.Helper;
 using WolvenKit.RED4.Types;
 using EFileReadErrorCodes = WolvenKit.RED4.Archive.IO.EFileReadErrorCodes;
 
@@ -13,69 +16,155 @@ namespace ResourceGenerator
 
         static void Main(string[] args)
         {
-            GenerateItemClasses(Path.Combine(_gameDir, "r6", "cache", "tweakdb.bin"));
+            GenerateTweakFiles(Path.Combine(_gameDir, "r6", "cache", "tweakdb.bin"));
             GenerateAppearanceValues(Path.Combine(_gameDir, "archive", "pc", "content", "basegame_1_engine.archive"));
         }
 
-        private static void GenerateItemClasses(string tweakPath)
+        private static void GenerateTweakFiles(string tweakPath)
         {
+            var stringHelper = new TweakDBStringHelper();
+            stringHelper.LoadFromStream(typeof(TweakDBService).Assembly.GetManifestResourceStream("WolvenKit.Common.Resources.tweakdbstr.kark"));
+            TweakDBIDPool.ResolveHashHandler += stringHelper.GetString;
+
             using var fh = File.OpenRead(tweakPath);
             using var reader = new TweakDBReader(fh);
 
             if (reader.ReadFile(out var tweakDb) == WolvenKit.RED4.TweakDB.EFileReadErrorCodes.NoError)
             {
-                var dict = new Dictionary<string, string>();
+                GenerateModList(tweakDb);
+                GenerateVehicles(tweakDb);
+                GenerateItemClasses(tweakDb);
+            }
+        }
 
-                foreach (var (id, type) in tweakDb.Records)
+        private static void GenerateModList(TweakDB tweakDb)
+        {
+            var dict = new Dictionary<string, Dictionary<ulong, List<ulong>>>
                 {
-                    var idStr = $"{id & 0xFFFFFFFF:X8}:{(id >> 32 & 0xFF):X2}";
+                    { "Clothing", new Dictionary<ulong, List<ulong>>() },
+                    { "Weapon", new Dictionary<ulong, List<ulong>>() }
+                };
 
-                    if (type == typeof(gamedataItem_Record))
+            foreach (var (id, type) in tweakDb.Records)
+            {
+                if (id.ResolvedText.StartsWith("Items") && type == typeof(gamedataItem_Record))
+                {
+                    var fullRecord = (gamedataItem_Record)tweakDb.GetFullRecord(id);
+                    if (fullRecord.Tags == null)
                     {
-                        dict.Add(idStr, "Item");
                         continue;
                     }
 
-                    if (type == typeof(gamedataClothing_Record))
+                    foreach (var tag in fullRecord.Tags)
                     {
-                        dict.Add(idStr, "Clothing");
-                        continue;
-                    }
+                        if (tag == "FabricEnhancer")
+                        {
+                            foreach (var placementSlot in fullRecord.PlacementSlots)
+                            {
+                                if (!dict["Clothing"].ContainsKey(placementSlot))
+                                {
+                                    dict["Clothing"].Add(placementSlot, new List<ulong>());
+                                }
 
-                    if (type == typeof(gamedataWeaponItem_Record))
-                    {
-                        dict.Add(idStr, "WeaponItem");
-                        continue;
-                    }
+                                if (!dict["Clothing"][placementSlot].Contains(id))
+                                {
+                                    dict["Clothing"][placementSlot].Add(id);
+                                }
+                            }
+                        }
 
-                    if (type == typeof(gamedataItemRecipe_Record))
-                    {
-                        dict.Add(idStr, "ItemRecipe");
-                        continue;
-                    }
+                        if (tag == "WeaponMod")
+                        {
+                            foreach (var placementSlot in fullRecord.PlacementSlots)
+                            {
+                                if (!dict["Weapon"].ContainsKey(placementSlot))
+                                {
+                                    dict["Weapon"].Add(placementSlot, new List<ulong>());
+                                }
 
-                    if (type == typeof(gamedataConsumableItem_Record))
-                    {
-                        dict.Add(idStr, "ConsumableItem");
-                        continue;
-                    }
-
-                    if (type == typeof(gamedataGrenade_Record))
-                    {
-                        dict.Add(idStr, "Grenade");
-                        continue;
-                    }
-
-                    if (type == typeof(gamedataGadget_Record))
-                    {
-                        dict.Add(idStr, "Gadget");
-                        continue;
+                                if (!dict["Weapon"][placementSlot].Contains(id))
+                                {
+                                    dict["Weapon"][placementSlot].Add(id);
+                                }
+                            }
+                        }
                     }
                 }
-
-                dict = dict.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
-                File.WriteAllText("ItemClasses.json", JsonSerializer.Serialize(dict, new JsonSerializerOptions { WriteIndented = true }));
             }
+
+            File.WriteAllText("Mods.json", JsonSerializer.Serialize(dict, new JsonSerializerOptions { WriteIndented = true }));
+        }
+
+        private static void GenerateVehicles(TweakDB tweakDb)
+        {
+            var list = new List<string>();
+
+            foreach (var (id, _) in tweakDb.Records)
+            {
+                var resolvedText = id.GetResolvedText();
+                if (resolvedText != null && resolvedText.StartsWith("Vehicle.") && resolvedText.EndsWith("_player"))
+                {
+                    list.Add(resolvedText);
+                }
+            }
+
+            list.Sort();
+            File.WriteAllText("Vehicles.json", JsonSerializer.Serialize(list, new JsonSerializerOptions { WriteIndented = true }));
+        }
+
+        private static void GenerateItemClasses(TweakDB tweakDb)
+        {
+            var dict = new Dictionary<string, string>();
+
+            foreach (var (id, type) in tweakDb.Records)
+            {
+                var idStr = $"{id & 0xFFFFFFFF:X8}:{(id >> 32 & 0xFF):X2}";
+
+                if (type == typeof(gamedataItem_Record))
+                {
+                    dict.Add(idStr, "Item");
+                    continue;
+                }
+
+                if (type == typeof(gamedataClothing_Record))
+                {
+                    dict.Add(idStr, "Clothing");
+                    continue;
+                }
+
+                if (type == typeof(gamedataWeaponItem_Record))
+                {
+                    dict.Add(idStr, "WeaponItem");
+                    continue;
+                }
+
+                if (type == typeof(gamedataItemRecipe_Record))
+                {
+                    dict.Add(idStr, "ItemRecipe");
+                    continue;
+                }
+
+                if (type == typeof(gamedataConsumableItem_Record))
+                {
+                    dict.Add(idStr, "ConsumableItem");
+                    continue;
+                }
+
+                if (type == typeof(gamedataGrenade_Record))
+                {
+                    dict.Add(idStr, "Grenade");
+                    continue;
+                }
+
+                if (type == typeof(gamedataGadget_Record))
+                {
+                    dict.Add(idStr, "Gadget");
+                    continue;
+                }
+            }
+
+            dict = dict.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+            File.WriteAllText("ItemClasses.json", JsonSerializer.Serialize(dict, new JsonSerializerOptions { WriteIndented = true }));
         }
 
         private static void GenerateAppearanceValues(string archivePath)
