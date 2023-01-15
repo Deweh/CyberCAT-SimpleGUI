76847379aeccdb99afdf932ea9977733b52f642e
seberoth <seberoth@users.noreply.github.com>
2023-01-15 14:13:31 +0100

    - Added Equipment-EX classes

---
+++ CP2077SaveEditor/ModSupport/EquipmentEx/OutfitPart.cs
@@ -0,0 +1,21 @@
+﻿using WolvenKit.RED4.Types;
+
+namespace CP2077SaveEditor.ModSupport;
+
+[RED("EquipmentEx.OutfitPart")]
+public class OutfitPart : gameScriptableSystem
+{
+    [RED("itemID")]
+    public gameItemID ItemID
+    {
+        get => GetPropertyValue<gameItemID>();
+        set => SetPropertyValue<gameItemID>(value);
+    }
+
+    [RED("slotID")]
+    public TweakDBID SlotID
+    {
+        get => GetPropertyValue<TweakDBID>();
+        set => SetPropertyValue<TweakDBID>(value);
+    }
+}
---
+++ CP2077SaveEditor/ModSupport/EquipmentEx/OutfitState.cs
@@ -0,0 +1,35 @@
+﻿using WolvenKit.RED4.Types;
+
+namespace CP2077SaveEditor.ModSupport;
+
+[RED("EquipmentEx.OutfitState")]
+public class OutfitState : gameScriptableSystem
+{
+    [RED("disabled")]
+    public CBool Disabled
+    {
+        get => GetPropertyValue<CBool>();
+        set => SetPropertyValue<CBool>(value);
+    }
+
+    [RED("active")]
+    public CBool Active
+    {
+        get => GetPropertyValue<CBool>();
+        set => SetPropertyValue<CBool>(value);
+    }
+
+    [RED("parts")]
+    public CArray<CHandle<OutfitPart>> Parts
+    {
+        get => GetPropertyValue<CArray<CHandle<OutfitPart>>>();
+        set => SetPropertyValue<CArray<CHandle<OutfitPart>>>(value);
+    }
+
+    [RED("outfits")]
+    public CArray<CHandle<OutfitSet>> Outfits
+    {
+        get => GetPropertyValue<CArray<CHandle<OutfitSet>>>();
+        set => SetPropertyValue<CArray<CHandle<OutfitSet>>>(value);
+    }
+}
